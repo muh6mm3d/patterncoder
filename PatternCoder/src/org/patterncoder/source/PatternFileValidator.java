0e1b07c09b937829095ad55f40cacdc6352abb24
jhpaterson <jhpaterson@45b873ec-3d32-0410-b865-7338eec619da>
2007-06-11 11:30:55 +0000

    Added GPL message to all source files, changed XML schema name to patternschema.xsd

--- PatternCoder/src/org/patterncoder/source/PatternFileValidator.java
+++ PatternCoder/src/org/patterncoder/source/PatternFileValidator.java
@@ -1,8 +1,20 @@
-/*
- * PatternFileValidator.java
- *
- * Date: 17 January 2006
- *
+/* Copyright (C) 2005 - 2007 the patternCoder team, http://www.patterncoder.org
+ 
+    This file is part of the patternCoder application
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.patterncoder.source;
@@ -21,8 +33,8 @@ import java.io.*;
 public class PatternFileValidator {
     
     static final String W3C_XML_SCHEMA ="http://www.w3.org/2001/XMLSchema"; 
-    static final String schemaSource = "lib/extensions/PatternFiles/bluedpschema.xsd";
-    static final String schemaSource2 = "patext/bluedpschema.xsd";
+    static final String schemaSource = "lib/extensions/PatternFiles/patternschema.xsd";
+    static final String schemaSource2 = "patext/patternschema.xsd";
     
     private Schema schema;
     private Validator validator;
